ab16d3bc15ad028ac3b0ac390cf5d13dc224ef34
tonynyyan <tonynyyan@tencent.com>
2026-09-05 08:40:14 +0800

    feat: add rdevd broker runtime entrypoint

---
+++ cmd/rdevd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/CIPFZ/rdev/internal/broker"
+)
+
+func main() {
+	socket := flag.String("socket", "", "Unix socket path")
+	flag.Parse()
+	if *socket == "" {
+		log.Fatal("-socket is required")
+	}
+	ln, err := broker.Listen(*socket)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	var ready broker.Readiness
+	ready.SetReady(true)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+			continue
+		}
+		_ = conn.Close()
+	}
+}
+
+var _ = os.Getuid
