c69f1fa9fdfc54ecc092e5db314b51ac27642003
tonynyyan <tonynyyan@tencent.com>
2026-09-05 08:44:24 +0800

    test: verify broker policy on rpc requests

--- cmd/rdevd/main_test.go
+++ cmd/rdevd/main_test.go
@@ -38,3 +38,29 @@ func TestServeConnRejectsIncompatible(t *testing.T) {
 		t.Fatal("incompatible hello accepted")
 	}
 }
+
+func TestServeConnAppliesPolicy(t *testing.T) {
+	a, b := net.Pipe()
+	defer a.Close()
+	service := broker.NewService(nil)
+	go serveConn(b, service)
+	_ = json.NewEncoder(a).Encode(proto.BrokerHello{Version: proto.BrokerProtocolVersion, MinVersion: proto.BrokerMinVersion})
+	var hello proto.BrokerHelloResponse
+	_ = json.NewDecoder(a).Decode(&hello)
+	owner := broker.Owner{ClientID: "c", ProjectID: "p"}
+	_ = json.NewEncoder(a).Encode(broker.Request{ID: "1", Owner: owner, Operation: "exec"})
+	var denied broker.Response
+	_ = json.NewDecoder(a).Decode(&denied)
+	if denied.OK {
+		t.Fatal("default policy allowed request")
+	}
+	if err := service.Grant(owner, "exec"); err != nil {
+		t.Fatal(err)
+	}
+	_ = json.NewEncoder(a).Encode(broker.Request{ID: "2", Owner: owner, Operation: "exec"})
+	var allowed broker.Response
+	_ = json.NewDecoder(a).Decode(&allowed)
+	if !allowed.OK {
+		t.Fatalf("granted request denied: %s", allowed.Error)
+	}
+}
